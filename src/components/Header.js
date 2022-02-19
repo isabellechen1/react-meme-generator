3b86fa29026d2109fdc8dfc30bcf6295faa58bff
isabellechen1 <isabellechen1120@gmail.com>
2022-02-19 13:40:21 +1100

    router

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img  
+                className="header--image"
+                alt="logo"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
